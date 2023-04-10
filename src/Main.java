7c27db376a87fe48485a7066773b73c34384ff09
Gökay Öncü <128924810+gokayoncu@users.noreply.github.com>
2023-04-10 15:37:34 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3, 4, 5};
+        double sum = 0;
+        for (int i = 0; i < list.length; i++) {
+            sum +=1/list[i];
+        }
+        System.out.println("Harmonik Ortalama: " + list.length / sum);
+    }
+}
